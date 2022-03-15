82a840f6db9a5b98b50ec7a1384c379cd944e862
Jintal <60089846+Jintal@users.noreply.github.com>
2022-03-15 23:07:08 +0530

    Open Board

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+// To find the index.html file
+app.use(express.static('public'));
+
+const port = 5500;
+const server = app.listen(port, function(){
+    console.log('Listening to port ', port);
+});
+
+
+const io = socket(server);
+
+// Whenever the front end connect to the server it will detect it and run the callback
+io.on("connection", function(socket){
+    console.log('Made socket connection from frontend');
+
+    // The data sent by the frontend should be broadcasted
+    socket.on('beginPath', function(data){
+        // Broadcast to all connected computers
+        io.sockets.emit('beginPath', data);
+    });
+
+    socket.on('drawStroke', function(data){
+        io.sockets.emit('drawStroke', data);
+    });
+
+    socket.on('redoUndo', function(data){
+        io.sockets.emit('redoUndo', data);
+    })
+});
